f000cd39f77926a798454c73f65591142142241a
Mia <mariamasha12345@gmail.com>
2025-01-24 20:50:45 +0300

    cleaning up navigation

--- app/src/main/java/com/manyacov/ratetrackerapp/navigation/BottomNavItem.kt
+++ app/src/main/java/com/manyacov/ratetrackerapp/navigation/BottomNavItem.kt
@@ -3,7 +3,7 @@ package com.manyacov.ratetrackerapp.navigation
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
-open class BottomNavItem(
+abstract class BottomNavItem(
     val path: String,
     @StringRes val titleRes: Int,
     @DrawableRes val iconRes: Int
---
+++ app/src/main/java/com/manyacov/ratetrackerapp/utils/Constants.kt
@@ -0,0 +1,3 @@
+package com.manyacov.ratetrackerapp.utils
+
+const val SCREEN_CHANGING_DURATION = 500
