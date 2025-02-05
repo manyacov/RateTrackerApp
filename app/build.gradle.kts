380b2ac668099e8bb0a338a783d6e58602fb5d34
Mia <mariamasha12345@gmail.com>
2025-02-05 18:58:30 +0300

    update targetSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.manyacov.ratetrackerapp"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
